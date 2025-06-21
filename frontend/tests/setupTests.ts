5bd9e39435c7e8d3e3e9f51b0172acab994c2982
Kimia Jamei <kimia.jamei@students.hs-mainz.de>
2025-06-21 19:26:20 +0200

    Map.test Datei wurde korrigiert

--- frontend/tests/setupTests.ts
+++ frontend/tests/setupTests.ts
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom';
+import fetchMock from 'jest-fetch-mock';
 import { TextEncoder, TextDecoder } from 'util';
+fetchMock.enableMocks();
 
 (global as any).TextEncoder = TextEncoder;
 (global as any).TextDecoder = TextDecoder;
